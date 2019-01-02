309c3eaa051b2cb15f03c6f07eea873d8da70de8
Rane Lafraze <lafrazerl@gmail.com>
2019-01-02 16:48:21 -0500

    Change GET to POST variables

--- server/mobile/team_action.php
+++ server/mobile/team_action.php
@@ -1,14 +1,5 @@
 <?php
 
-// Check to make sure the parameter exists / is valid
-// CLEANUP: Uncomment
-// if((!isset($_POST["intent"])) || (!isset($_POST("account_email)))) {
-//     header("Location: https://www.sportwatch.us");
-//     exit();
-// }
-
-// Cleanup / TODO: Impletemnet SID check
-
 /**
  * HOW TO USE
  * Summary:
@@ -68,33 +59,40 @@ require("../wp-content/themes/primer/sw-db.php");
 $db = new DatabaseConnection();
 $CODE_LENGTH = 7; // Base length of random generated code
 
+// -- Check their SID -- //
+if((getVar("SID") == false) || (!$db->validateSID(getVar("SID")))) {
+    sendResponse(array("status" => -5, "substatus" => 2, "info" => "invalid SID"));
+}
+// Will check the SID against the user id below
+$userIdFromSID = $db->selectSingle("SELECT id_user FROM user_session_data WHERE SID = ?", "s", getVar("SID"));
+$userIdFromSID = $userIdFromSID[0];
+
+// ---- GRAB THE VARIABLES ---- //
 $intent = getVar("intent");
 $accountId = -1; // Account initiating the call
 $schoolId = -1; // Team identifying variables below
-$inviteCode = getVar("invite_code");
-$teamName = getVar("team_name");
+$inviteCode = preg_replace("/[^A-Za-z0-9]/", "", getVar("invite_code"));
+$teamName = preg_replace("/[^A-Za-z0-9_]/", "", getVar("team_name"));
 $primaryCoachEmail = getVar("primary_coach");
 $primaryCoachId = -1;
 $secondaryCoachEmail = getVar("secondary_coach");
 $secondaryCoachId = -1;
 
-// $db->insertValues("team", array("invite_code" => "ffj3ds5", "team_name" => "Hemlock", "id_school" => 2,
-//                                "id_coach_primary" => 2, "id_coach_secondary" => 1, "is_locked" => 1));
-
-// $db->modifyRecord("team", array("id_coach_secondary" => 5), "id_team", 5);
-// die();
-
 if($intent === false) {
-    sendResponse(array("status" => -5, "substatus" => 3, "info" => "no intent"));
+    sendResponse(array("status" => -5, "substatus" => 4, "info" => "no intent"));
 }
 if(!getVar("account_email")) {
-    sendResponse(array("status" => -5, "substatus" => 3, "info" => "no user email"));
+    sendResponse(array("status" => -5, "substatus" => 4, "info" => "no user email"));
 }
 $accountId = $db->selectSingle("SELECT id_user FROM user WHERE email = ?", "s", getVar("account_email"));
 if((count($accountId) > 0) && ($db->emailExists(getVar("account_email")))) {
     $accountId = $accountId[0];
 } else {
-    sendResponse(array("status" => -5, "substatus" => 7, "info" => "provided email: " . getVar("account_email")));
+    sendResponse(array("status" => -5, "substatus" => 8, "info" => "provided email: " . getVar("account_email")));
+}
+// Check the SID against user ID
+if($accountId != $userIdFromSID) {
+    sendResponse(array("status" => -5, "substatus" => 2, "info" => "invalid SID"));
 }
 
 // ---- PARSE TEAM IDENTIFIER VARIABLES ---- //
@@ -111,7 +109,7 @@ if($schoolName != false) {
 if(strlen($inviteCode) != $CODE_LENGTH) {
     $inviteCode = "NULL";
 }
-if(strlen($teamName) <= 5) {
+if((strlen($teamName) <= 5) || (strlen($teamName) > 75)) {
     $teamName = "";
 }
 if($primaryCoachEmail != NULL) {
@@ -134,10 +132,10 @@ if($secondaryCoachEmail != false) {
 // ---- TRY FINDING THE TEAM WITH BEST MATCH TO PROVIDED INFO ---- //
 
 $targetTeamId = getVar("id_team");
-if($targetTeamId !== false) { // TODO: Make sure this won't flag a id = 0
+if($targetTeamId !== false) {
     // If the team doesn't exists, error
     if(count($db->selectSingle("SELECT invite_code FROM team WHERE id_team = ?", "i", $targetTeamId)) < 1) {
-        sendResponse(array("status" => -5, "substatus" => 6, "info" => "invalid team id"));
+        sendResponse(array("status" => -5, "substatus" => 7, "info" => "invalid team id"));
     }
 } else {
     
@@ -225,7 +223,7 @@ if($targetTeamId !== false) { // TODO: Make sure this won't flag a id = 0
         } else {
             $sameScore = "false";
         }
-        sendResponse(array("status" => -5, "substatus" => 5, "info" => 
+        sendResponse(array("status" => -5, "substatus" => 6, "info" => 
                            "accuracy = {$pastScore} of 8. duplicates: {$sameScore}"));
     }
 }
@@ -289,7 +287,7 @@ if($intent == 0) {
         $coachArray["secondary_coach"] = $secondaryName;
     }
     // And then, ya just gotta send it...
-    if(count($enrolledName) > 0) {
+    if((count($enrolledName) > 0) || (count($coachArray) > 0)) {
         sendResponse(array("status" => 5, "substatus" => 0, "info" => "na", "athletes" => $enrolledName) + $coachArray);
     } else {
         sendResponse(array("status" => -5, "substatus" => 0, "info" => "team had no members"));
@@ -317,14 +315,14 @@ if($intent == 0) {
             $success = $db->joinTeam($accountId, $targetTeamId);
         }
     } else {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "team is locked"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "team is locked"));
     }
     
 // -- Leave team -- //
 } else if($intent == 4) {
     // Make sure the user is actually a part of the team
     if(!$db->isInTeam($accountId, $targetTeamId)) {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "user not in team"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "user not in team"));
     }
     // If there is another coach, make them the primary coach now
     if($accountId == $teamData["id_coach_primary"]) {
@@ -333,7 +331,7 @@ if($intent == 0) {
                                                        "id_coach_Secondary" => NULL), "id_team", $targetTeamId);
         } else {
             // A team can't be coach-less, so they can't leave
-            sendResponse(array("status" => -5, "substatus" => 2, "info" => "only coach cannot leave"));
+            sendResponse(array("status" => -5, "substatus" => 3, "info" => "only coach cannot leave"));
         }
         
     } else if($accountId == $teamData["id_coach_secondary"]) {
@@ -344,7 +342,7 @@ if($intent == 0) {
             $success = $db->modifyRecord("team", array("id_coach_primary" => $teamData["id_coach_secondary"],
                                                        "id_coach_secondary" => NULL), "id_team", $targetTeamId);
             // A team can't be coach-less, so they can't leave
-            sendResponse(array("status" => -5, "substatus" => 2, "info" => "only coach cannot leave"));
+            sendResponse(array("status" => -5, "substatus" => 3, "info" => "only coach cannot leave"));
         }
     }
     // Remove their id_team value from user table
@@ -365,20 +363,20 @@ if($intent == 0) {
     }
     // Make sure the initiated user is a coach. If not, error
     if(($accountId != $teamData["id_coach_primary"]) && ($accountId != $teamData["id_coach_secondary"])) {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "coach-only action"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "coach-only action"));
     }
     if($kickedId < 0) {
-        sendResponse(array("status" => -5, "substatus" => 3, "info" => 
+        sendResponse(array("status" => -5, "substatus" => 4, "info" => 
                            "kicked_email was invalid: " . getVar("kicked_email")));
     }
     // Make sure kicked user is a part of the forbidden team
     if(!$db->isInTeam($kickedId, $targetTeamId)) {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => 
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => 
                            "user " . getVar("kicked_email") . " is not in team " . $teamData["team_name"]));
     }
     // You can't overthrow the kind (primary coach)
     if($kickedId == $teamData["id_coach_primary"]) {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "cannot kick primary coach"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "cannot kick primary coach"));
     }
     // Is the secondary coach being banished?
     if(($accountId == $teamData["id_coach_primary"]) && ($kickedId == $teamData["id_coach_secondary"])) {
@@ -399,11 +397,11 @@ if($intent == 0) {
         }
     }
     if($promotedId < 0) {
-        sendResponse(array("status" => -5, "substatus" => 3, "info" => 
+        sendResponse(array("status" => -5, "substatus" => 4, "info" => 
                            "promoted_email was invalid: " . getVar("promoted_email")));
     }
     if($promotedId == $teamData["id_coach_primary"]) {
-        sendResponse(array("status" => 5, "substatus" => 2, "info" => "already primary coach"));
+        sendResponse(array("status" => 5, "substatus" => 3, "info" => "already primary coach"));
     }
     
     if($accountId == $teamData["id_coach_primary"]) {
@@ -416,7 +414,7 @@ if($intent == 0) {
                                                                  "id_coach_secondary" => $teamData["id_coach_secondary"]),
                                                   "id_team", $targetTeamId));
     } else {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "primary coach-only action"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "primary coach-only action"));
     }
     
 // -- Appoint secondary coach -- //
@@ -434,11 +432,11 @@ if($intent == 0) {
     if(($accountId == $teamData["id_coach_primary"]) || ($accountId == $teamData["id_coach_secondary"])) {
         // Do some safety checks here
         if($promotedId < 0) {
-            sendResponse(array("status" => -5, "substatus" => 3, "info" => 
+            sendResponse(array("status" => -5, "substatus" => 4, "info" => 
                                "promoted_email was invalid: " . getVar("promoted_email")));
         }
         if(($promotedId == $teamData["id_coach_primary"]) || ($promotedId == $teamData["id_coach_secondary"])) {
-            sendResponse(array("status" => 5, "substatus" => 2, "info" => "already a coach"));
+            sendResponse(array("status" => 5, "substatus" => 3, "info" => "already a coach"));
         }
         
         // Make the promoted user a part of the team
@@ -446,7 +444,7 @@ if($intent == 0) {
         $success = ($success) && ($db->modifyRecord("team", array("id_coach_secondary" => $promotedId), 
                                                                  "id_team", $targetTeamId));
     } else {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "coach-only action"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "coach-only action"));
     }
     
 // -- Demote coach -- //
@@ -461,12 +459,12 @@ if($intent == 0) {
         }
     }
     if($demotedId < 0) {
-        sendResponse(array("status" => -5, "substatus" => 3, "info" => 
+        sendResponse(array("status" => -5, "substatus" => 4, "info" => 
                            "demoted_email was invalid: " . getVar("demoted_email")));
     }
     // Is the demoted user is a coach?
     if(($demotedId != $teamData["id_coach_primary"]) && ($demotedId != $teamData["id_coach_secondary"])) {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" =>
+        sendResponse(array("status" => -5, "substatus" => 3, "info" =>
                            getVar("demoted_email") . " is not a coach in team " . $teamData["team_name"]));
     }
     // Make sure the primary coach is the one demoting
@@ -477,7 +475,7 @@ if($intent == 0) {
                                                            "id_coach_secondary" => $teamData["id_coach_primary"]),
                                             "id_team", $targetTeamId);
             } else {
-                sendResponse(array("status" => -5, "substatus" => 2, "info" => "team must have primary coach"));
+                sendResponse(array("status" => -5, "substatus" => 3, "info" => "team must have primary coach"));
             }
         } else if($demotedId == $teamData["id_coach_secondary"]) {
             $success = $db->modifyRecord("team", array("id_coach_secondary" => NULL), "id_team", $targetTeamId);
@@ -485,14 +483,14 @@ if($intent == 0) {
     // If the secondary coach is doing the demoting...
     } else if($accountId == $teamData["id_coach_secondary"]) {
         if($demotedId == $teamData["id_coach_primary"]) {
-            sendResponse(array("status" => -5, "substatus" => 2, "info" => "secondary coach cannot demote primary coach"));
+            sendResponse(array("status" => -5, "substatus" => 3, "info" => "secondary coach cannot demote primary coach"));
         } else if(($demotedId == $teamData["id_coach_secondary"]) && ($teamData["id_coach_primary"] != NULL)) {
             $success = $db->modifyRecord("team", array("id_coach_secondary" => NULL), "id_team", $targetTeamId);
         } else {
-            sendResponse(array("status" => -5, "substatus" => 2, "info" => "team cannot be coach-less"));
+            sendResponse(array("status" => -5, "substatus" => 3, "info" => "team cannot be coach-less"));
         }
     } else { // And if the user wasn't even a coach
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "coach-only action"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "coach-only action"));
     }
     
 // -- Lock team -- //
@@ -508,14 +506,14 @@ if($intent == 0) {
             sendResponse(array("status" => 5, "substatus" => 0, "info" => "na", "is_locked" => $lockState));
         }
     } else {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "coach-only action"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "coach-only action"));
     }
     
 // -- Change team name -- //
 } else if($intent == 10) {
     $newName = getVar("new_name");
-    if(strlen($newName) <= 5) {
-        sendResponse(array("status" => -5, "substatus" => 3, "info" => "invalid or short name: " . $newName));
+    if((strlen($newName) <= 5) || (strlen($newName) > 75)) {
+        sendResponse(array("status" => -5, "substatus" => 4, "info" => "invalid or short or long name: " . $newName));
     }
     if(($accountId == $teamData["id_coach_primary"]) || ($accountId == $teamData["id_coach_secondary"])) {
         $success = $db->modifyRecord("team", array("team_name" => $newName), "id_team", $targetTeamId);
@@ -523,7 +521,7 @@ if($intent == 0) {
             sendResponse(array("status" => 5, "substatus" => 0, "info" => "na", "team_name" => $newName));
         }
     } else {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "coach-only action"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "coach-only action"));
     }
     
 // -- Change / re-generate invite code -- //
@@ -549,7 +547,7 @@ if($intent == 0) {
         }
         
     } else {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "coach-only action"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "coach-only action"));
     }
     
 // -- Delete team -- //
@@ -562,12 +560,12 @@ if($intent == 0) {
         
         $success = $db->deleteRecord("team", array("id_team" => $targetTeamId));
     } else {
-        sendResponse(array("status" => -5, "substatus" => 2, "info" => "primary coach-only action"));
+        sendResponse(array("status" => -5, "substatus" => 3, "info" => "primary coach-only action"));
     }
     
 // -- Uknown intent -- //
 } else {
-    sendResponse(array("status" => -5, "substatus" => 4, "info" => "unknown intent"));
+    sendResponse(array("status" => -5, "substatus" => 5, "info" => "unknown intent"));
 }
 
 
@@ -689,10 +687,9 @@ function createCode($length, $litter = "", $seed = "") {
  * getVar("boundy_ball_7"); --> false
  * 
  * @param String $varName - identifier of the variable
- * @param Integer $varSource - [default = 1] 0 for POST, 1 for GET variable
+ * @param Integer $varSource - [default = 0] 0 for POST, 1 for GET variable
  */
-function getVar($varName, $varSource = 1) {
-    // CLEANUP: Change 1 to 0, aka GET to POST
+function getVar($varName, $varSource = 0) {
     
     $varValue = NULL;
     if(($varSource == 0) && (isset($_POST[$varName]))) {
