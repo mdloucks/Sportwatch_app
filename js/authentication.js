9333a87758d906106f56969b416271c3eb15dad3
Rane Lafraze <lafrazerl@gmail.com>
2019-01-22 18:33:16 -0500

    Add no Internet connetion error (temporary)

--- js/authentication.js
+++ js/authentication.js
@@ -84,6 +84,7 @@ var Authentication = {
                 },
                 error: (response) => {
                     // TODO: redo this to handle no internet connection
+                    $("#app").html(`<p>Invalid SID or Connection Error</p>`);
                     let data = JSON.parse(response);
                     ErrorHandler.handleAjaxError(data);
                     reject(data);
