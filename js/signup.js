3aa24296fd54d81f42deeef5e533ef6d508d0120
Rane Lafraze <lafrazerl@gmail.com>
2019-05-10 21:41:13 -0400

    Update styling operation and begin styling signup

--- js/signup.js
+++ js/signup.js
@@ -3,33 +3,41 @@
 function signupPage() {
     return new Promise((resolve, reject) => {
 
-        // back arrow
-        $("header").prepend(`
-            <span style="font-size: 2em;">&#8592</span>
-        `);
-
-        $("header > span").click(function (e) {
-            e.preventDefault();
-            resolve("welcome");
-        });
-
         $("#app").html(`
+            <br><br>
+            <h1>Sportwatch</h1>
+            <br>
+            <span class="back_arrow">&#8592</span>
+            <p>Please enter your information below</p>
             <form>
-                <label for="email">Email</label>
-                <input type='email' name='email'><br>
-                <label for="password">Password</label>
-                <input type='password' name='password'><br>
-                <label for="coach">Coach</label>
+                <div class="form_holder">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+                </div> <br>
+                <div class="form_holder">
+                    <label for="password">Password</label>
+                    <input type="password" name="password">
+                </div> <br>
+                <p>I am a...</p>
+                <button id="athlete" class="account_type_button" type="button">Athlete</button>
+                <button id="coach" class="account_type_button" type="button">Coach</button>
+                <!-- <label for="coach">Coach</label>
                 <input type="radio" name="account_type" value="coach">
                 <label for="athlete">Athlete</label>
-                <input type="radio" name="account_type" value="athlete">
-                <input type='submit' value='Sign Up'>
+                <input type="radio" name="account_type" value="athlete"> -->
+                <input type="submit" value="Sign Up">
             </form>
         `);
 
-        CSSManager.styleLoginPage();
-
-
+        //CSSManager.styleLoginPage();
+        CSSManager.resetStyling();
+        CSSManager.addStylesheet("signup.css");
+        
+        $(".back_arrow").click(function (e) {
+            e.preventDefault();
+            resolve("welcome");
+        });
+        
         $("form").on("submit", function (e) {
             e.preventDefault();
 
