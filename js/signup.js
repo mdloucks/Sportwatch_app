1fa6cf874129e2f8d1d95fbdfea7a5a267829679
Rane Lafraze <lafrazerl@gmail.com>
2019-05-21 20:51:26 -0400

    Finish styling signup, comment, add to-do's

--- js/signup.js
+++ js/signup.js
@@ -2,7 +2,10 @@
 
 function signupPage() {
     return new Promise((resolve, reject) => {
-
+        
+        CSSManager.resetStyling();
+        CSSManager.addStylesheet("signup.css");
+        
         $("#app").html(`
             <br><br>
             <h1>Sportwatch</h1>
@@ -10,32 +13,43 @@ function signupPage() {
             <span class="back_arrow">&#8592</span>
             <p>Please enter your information below</p>
             <form>
-                <div class="form_holder">
-                    <label for="email">Email</label>
-                    <input type="email" name="email">
-                </div> <br>
-                <div class="form_holder">
-                    <label for="password">Password</label>
-                    <input type="password" name="password">
-                </div> <br>
-                <p>I am a...</p>
-                <button id="athlete" class="account_type_button" type="button">Athlete</button>
+                <label id="label_email" for="email">Email: &nbsp</label>
+                <input class="sw_text_input" type="email" name="email">
+                <br>
+                <label id="label_password" for="password">Password: &nbsp;</label>
+                <input class="sw_text_input" type="password" name="password">
+                <br>
+                
+                <p id="p_accountType">I am a...</p>
+                <button id="athlete" class="account_type_button selected" type="button">Athlete</button>
                 <button id="coach" class="account_type_button" type="button">Coach</button>
+                <br><br><br>
                 <!-- <label for="coach">Coach</label>
                 <input type="radio" name="account_type" value="coach">
                 <label for="athlete">Athlete</label>
                 <input type="radio" name="account_type" value="athlete"> -->
-                <input type="submit" value="Sign Up">
+                
+                <input id="button_signup" class="sw_big_button" type="submit" value="Sign Up">
             </form>
         `);
-
-        //CSSManager.styleLoginPage();
-        CSSManager.resetStyling();
-        CSSManager.addStylesheet("signup.css");
+        // TODO: Add place for error message
+        
+        
+        // Use set timeout to prevent page moving upon load (https://bit.ly/2Qf7PS9)
+        setTimeout(function() {
+            $(".account_type_button").css("transition", "border 1s");
+        }, 1000);
         
         $(".back_arrow").click(function (e) {
             e.preventDefault();
             resolve("welcome");
+        });
+        
+        // "Radio button" logic for account types
+        $(".account_type_button").click((e) => {
+            // Remove .selected from both buttons
+            $(".account_type_button").removeClass("selected");
+            $(e.target).addClass("selected");
         });
         
         $("form").on("submit", function (e) {
@@ -43,12 +57,13 @@ function signupPage() {
 
             var email = $("input[type=email]").val();
             var password = $("input[type=password]").val();
-            var account_type = $("input[name=account_type]:checked").val();
+            var account_type = $(".account_type_button[class*=selected]").text().toLowerCase();
             // TODO PASSWORD STRENGTH TEST IS WEIRD
             Authentication.signup(email, password, account_type).then((response) => {
                 localStorage.setItem("email", email);
                 localStorage.setItem("account_type", account_type);
                 resolve("postsignup");
+                // TODO: Finish / implement post signup. Errors out right now
             }).catch(function (error) {
                 reject(error);
             });
