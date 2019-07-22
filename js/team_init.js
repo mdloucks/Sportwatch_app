83667535714d06d08a64eef4200522644a272fb6
Rane Lafraze <lafrazerl@gmail.com>
2019-07-22 07:24:31 -0400

    Add button effects

--- js/team_init.js
+++ js/team_init.js
@@ -3,23 +3,55 @@ function initTeamPage() {
     CSSManager.resetStyling();
     // No .css file since this page is so small
     
+    // ---- CALLBACK / STATE BIND FUNCTIONS ---- //
+
+    this.joinTeam = function () {
+        throw new Error("JOIN TEAM IS NOT SETUP");
+    }
+    this.onJoinTeam = (callback) => {
+        this.joinTeam = callback;
+    }
+
+    this.createTeam = function () {
+        throw new Error("CREATE TEAM IS NOT SETUP");
+    }
+    this.onCreateTeam = function (callback) {
+        this.createTeam = callback;
+    }
+    
+    // ---- PAGES / FUNCTIONALITY ---- //
+    
     $("#app").html(`
         <br>
         <input id="join_team" type="image" src="img/joinTeam.png" 
-            style="width: 250px; height: 200px; margin: 5px; padding: 15px; border: solid 2px gray; border-radius: 15px;"></input>
+            style="width: 250px; height: 200px; margin: 5px; padding: 15px; border: solid 2px gray; border-radius: 15px; transition: background-color 0.25s;"></input>
         <input id="create_team" type="image" src="img/createTeam.png" 
-            style="width: 250px; height: 200px; margin: 5px; padding: 15px; border: solid 2px gray; border-radius: 15px;"></input>
+            style="width: 250px; height: 200px; margin: 5px; padding: 15px; border: solid 2px gray; border-radius: 15px; transition: background-color 0.25s;"></input>
     `);
     
     // TODO Implement page changes here when the buttons are clicked
-    $("#app").on("click", "#join_team", function(e) {
+    $("#app").on("click", "#join_team", (e) => {
         e.preventDefault();
-        console.log("Joined");
+        $("#join_team").css("background-color", "lightgray");
+        
+        setTimeout(() => {
+            $("#join_team").css("background-color", "white");
+        }, 250);
+        setTimeout(() => {
+            this.joinTeam();
+        }, 300);
     });
     
-    $("#app").on("click", "#create_team", function(e) {
+    $("#app").on("click", "#create_team", (e) => {
         e.preventDefault();
-        console.log("Created");
+        $("#create_team").css("background-color", "lightgray");
+        
+        setTimeout(() => {
+            $("#create_team").css("background-color", "white");
+        }, 250);
+        setTimeout(() => {
+            this.createTeam();
+        }, 300);
     });
     
 }
