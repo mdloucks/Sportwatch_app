c441bc0df30baba18ef893a61303572dbe9200c9
Rane Lafraze <lafrazerl@gmail.com>
2019-11-15 15:15:17 -0500

    Add tips to team name page and function for school info page

--- js/team_create.js
+++ js/team_create.js
@@ -21,16 +21,39 @@ function createTeamPage() {
             <h1 id="h1_giveName">Name the Team</h1>
             <input id="team_name" class="sw_text_input" type="text"></input>
             <input id="button_submitName" type="image" src="img/arrow.png"></input>
+            <br><br>
+            <p id="p_tipHeading">Naming Tips:</p><br>
+            <ul class="ul_tips">
+                <li class="tips bolded">Use 15-45 characters</li>
+                <li class="tips">Avoid special characters</li>
+                <li class="tips">Create a unique name</li>
+                <li class="tips">Capitalize names</li>
+            </ul>
+        </div>
+    `);
+    
+    let schoolPage = (`
+        <div id="schoolPage" class="div_page">
+            <h1 id="h1_schoolName">Team's School</h1>
         </div>
     `);
     
     
+    // Functionality
+    $("#app").on("click", "#button_submitName", (e) => {
+        e.preventDefault();
+        transitionObj.slideLeft("schoolPage");
+    });
+    
+    
+    // Final additions of pages
     
     $("#app").html("");
     $("#app").html(basePage);
     
     // Add the function pages to the transition manager
     transitionObj.addPage("teamName", namePage, true);
+    transitionObj.addPage("schoolPage", schoolPage);
     
     
     
