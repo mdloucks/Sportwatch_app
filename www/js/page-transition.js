f67f4f437fab1a6da5976d0ba60c5613911988ed
Rane Lafraze <lafrazerl@gmail.com>
2020-05-03 21:49:08 -0400

    Change to class, add support for embedded transitions

--- www/js/page-transition.js
+++ www/js/page-transition.js
@@ -4,157 +4,153 @@
  * Used for page transitions, including sliding and fade effects.
  * NOTE: the id of the div_page and the key for that page MUST match! If they
  * don't, it will be corrected for.
- * 
+ *
  * @example
  * let accountTransitions = new PageTransition("settings", "<p>Settings Page</p><button>Click to activate</button>");
  * accountTransitions.addPage("create_team", "<p>Create your awesome, unique team here!</p>");
  * accountTransitions.slideRight("create_team", 500);
  * 
+ * @param {String} sourceEl default = "". element which new pages will be added to (ex "#app")
  * @param {String} firstPageHtml default = "". first / default page html content
- *  * @param {String} firstPageKey default = "".  first / default page displayed name
-
+ * @param {String} firstPageKey default = "".  first / default page displayed name
  */
-function PageTransition(firstPageHtml = "", firstPageKey = "") {
+class PageTransition {
     
-    // All page keys should NOT include the #. When referring to pages with
-    // jQuery, add in a #.  All keys should also be lowercase
-    let pages = new Map();
-    let currentPage = "";
-    
-    // Attempt to add the default page if given
-    if(firstPageHtml !== "") {
-        this.addPage(firstPageKey, firstPageHtml, true);
+    constructor(sourceEl = "", firstPageHtml = "", firstPageKey = "") {
+        // All page keys should NOT include the #. When referring to pages with
+        // jQuery, add in a #.  All keys should also be lowercase
+        this.pages = new Map();
+        this.currentPage = "";
+        this.sourceElement = ""; // Usually #app or parent page div
+        // Set source element, or at least assume something
+        if (sourceEl !== "") {
+            this.sourceElement = "#" + sourceEl.replace("#", "");
+        } else {
+            this.sourceElement = "#app";
+        }
+        // // Attempt to add the default page if given
+        if(firstPageHtml !== "") {
+            this.addPage(firstPageKey, firstPageHtml, true);
+        }
     }
-    
     /**
      * Adds a page to the pages map for use when transitioning
      * NOTE: the id of the div_page and the key for that page MUST match! If they
      * don't, it will be corrected for.
-     * 
+     *
      * @example
      * addPage("coach", "<div id="coach" class="div_page"> <h1>Coach's Stop Here!</h1> </div>);
-     * 
+     *
      * @param {String} pageKey key for this page
      * @param {String} pageHtml html content for the new page
      * @param {Boolean} showPage should the page be shown after it is added?
      */
-    this.addPage = function(pageKey, pageHtml, showPage = false) {
-        
+    addPage(pageKey, pageHtml, showPage = false) {
         // Make sure these values were set
         if((pageKey !== "") && (pageHtml !== "")) {
-
             // This is the provided div_page id found in pageHtml
             // It needs to match pageKey to work correctly
             let rawPageId = this.verifyPageKey(pageHtml);
             pageKey = pageKey.replace("#", ""); // Remove any hashtags
-                        
             // If the div_page is missing, wrap the content in it
             if(!pageHtml.includes("div_page")) {
                 pageHtml = "<div id=\"" + pageKey + "\" class=\"div_page\">" + pageHtml + "</div>";
-                
-            // If the div_page is missing an id, add one
+                // If the div_page is missing an id, add one
             } else if(rawPageId == "") {
                 let classIndex = pageHtml.indexOf("div_page");
                 let divIndex = pageHtml.lastIndexOf("<div", classIndex) + 4;
                 // + 4 to remove <div from processing
-                
                 pageHtml = pageHtml.substring(0, divIndex) + " id=\"" + pageKey + "\"" + pageHtml.substring(divIndex, pageHtml.length);
-                
-            // If the div_page id doesn't match the key, correct it
+                // If the div_page id doesn't match the key, correct it
             } else if(pageKey != rawPageId) {
                 let currentIdIndex = pageHtml.indexOf(rawPageId);
                 let endIdIndex = currentIdIndex + rawPageId.length;
                 pageHtml = pageHtml.substring(0, currentIdIndex) + pageKey + pageHtml.substring(endIdIndex, pageHtml.length);
             }
-                        
-            pages.set(pageKey, pageHtml);
-            $("#app").append(pageHtml);
+            this.pages.set(pageKey, pageHtml);
+            
+            $(this.sourceElement).append(pageHtml);
             // Keep as base starting point for simplicity
             $("#" + pageKey).addClass("current_page");
-            if (!showPage) {
+            if(!showPage) {
                 // $(divId).addClass("page_right");
                 $("#" + pageKey).addClass("hidden");
             } else {
-                currentPage = pageKey;
+                this.currentPage = pageKey;
             }
-
         } else if(pageHtml !== "") {
             if(this.verifyPageKey(pageHtml) !== "") {
                 // Set the page with its id
-                pages.set(this.verifyPageKey(pageHtml), pageHtml);
+                this.pages.set(this.verifyPageKey(pageHtml), pageHtml);
             }
             // Else don't set since the pageHtml isn't given
-            
         } else {
             console.log("[PageTransition][addPage()]: Unable to add page due to missing parameters");
         }
     }
-    
     /**
      * Removes a page from the pages Map based on key value.
-     * 
+     *
      * @example
      * removePage("createTeam");
-     * 
+     *
      * @param {String} pageKey name of the page to be removed
-     * 
+     *
      * @return {Boolean} True, if success. False, if the page key didn't exist
      */
-    this.removePage = function(pageKey) {
-        return pages.delete(pageKey);
+    removePage(pageKey) {
+        return this.pages.delete(pageKey);
     }
-    
     /**
      * Slides the new page in from the right TO the left.
-     * 
+     *
      * @example
      * slideLeft("accountSettings", 500);
-     * 
+     *
      * @param {String} targetPageKey lowercase string of new page id
      * @param {Integer} duration [default = 1000] delay of transition play
      */
-    this.slideLeft = function(targetPageKey, duration = 1000) {
+    slideLeft(targetPageKey, duration = 1000) {
         this.slidePage(targetPageKey, true, duration);
     }
-    
     /**
      * Slides the new page in from the left TO the right.
-     * 
+     *
      * @example
      * slideRight("mainPage", 500);
-     * 
+     *
      * @param {String} targetPageKey lowercase string of new page id
      * @param {Integer} duration [default = 1000] delay of transition play
      */
-    this.slideRight = function(targetPageKey, duration = 1000) {
+    slideRight(targetPageKey, duration = 1000) {
         this.slidePage(targetPageKey, false, duration);
     }
-    
     /**
      * Internal method used to slide a page left or right, depending on the
      * second boolean parameter. slideLeft() and slideRight() should be
      * used instead of this function.
-     * 
+     *
      * @param {String} targetPageKey the target page name (in pages Map) of
      * the new page
      * @param {Boolean} slideLeft should the page slide to the left? (i.e. right to left)
      * @param {Integer} duration the duration of the transition, in milliseconds
      */
-    this.slidePage = function(targetPageKey, slideLeft, duration) {
-                
+    slidePage(targetPageKey, slideLeft, duration) {
         // Create ID's for previous and new page after removing any sneaky #'s
-        let prevPageId = "#" + currentPage.replace("#", "");
+        let prevPageId = "#" + this.currentPage.replace("#", "");
         let targetPageId = "#" + targetPageKey.replace("#", "");
-                
         // Prevent the double clicking of the button
-        if (($(prevPageId).is(":animated")) || ($(targetPageId).is(":animated"))) {
+        if(($(prevPageId).is(":animated")) || ($(targetPageId).is(":animated"))) {
             return;
         }
-        if (prevPageId == targetPageId) {
-            console.log("[PageTransition][slideLeft()]: Duplicate! New current page: " + currentPage);
+        if(prevPageId == targetPageId) {
+            console.log("[PageTransition][slidePage()]: Duplicate! New current page: " + this.currentPage);
             return;
         }
-                
+        // Remove any previous sliding CSS
+        $(targetPageId).css("left", "");
+        $(prevPageId).css("left", "");
+        
         // Set up location (opposite of direction of the slide)
         $(".div_page").css("transition", "left 0s");
         if(slideLeft) {
@@ -167,11 +163,10 @@ function PageTransition(firstPageHtml = "", firstPageKey = "") {
         setTimeout(() => {
             $(".div_page").css("transition", "left " + (duration / 1000) + "s");
         }, 1);
-        
         // Use setTimeout to avoid instant appearance of new page
         // Kind of jimmy-rigged, but I don't know how to solve the root problem :(
         setTimeout(() => {
-            if (slideLeft) {
+            if(slideLeft) {
                 $(targetPageId).removeClass("hidden");
                 $(targetPageId).removeClass("page_right");
                 $(prevPageId).addClass("page_left");
@@ -181,7 +176,6 @@ function PageTransition(firstPageHtml = "", firstPageKey = "") {
                 $(prevPageId).addClass("page_right");
             }
         }, 2);
-        
         // Hide old page once new page is in focus
         setTimeout(() => {
             $(prevPageId).addClass("hidden");
@@ -192,39 +186,81 @@ function PageTransition(firstPageHtml = "", firstPageKey = "") {
             } else {
                 $(prevPageId).removeClass("page_right");
             }
-            currentPage = targetPageId.replace("#", "");
+            this.currentPage = targetPageId.replace("#", "");
         }, duration);
-        
     }
     
+    /**
+     * Moves the target page the given value in pixels. This is NOT
+     * a full transition, but rather, should be called in conjunction
+     * with a touchmove event to update page appearance
+    */
+    slidePageX(targetPageKey, slideLeft, dx) {
+        
+        // Find ID's for previous and new page
+        let prevPageId = "#" + this.currentPage.replace("#", "");
+        let targetPageId = "#" + targetPageKey.replace("#", "");
+        // Prevent the double clicking of the button
+        if (($(prevPageId).is(":animated")) || ($(targetPageId).is(":animated"))) {
+            return;
+        }
+        // Set up location (opposite of direction of the slide)
+        $(".div_page").css("transition", "left 0s");
+        if (slideLeft) {
+            $(targetPageId).addClass("page_right");
+        } else {
+            $(targetPageId).addClass("page_left");
+        }
+        
+        if(dx == 0) {
+            $(".div_page").addClass("hidden");
+            $(prevPageId).removeClass("hidden"); // Reset all styling
+            $(targetPageId).css("left", "");
+            $(prevPageId).css("left", "");
+            return; // Clear the CSS transition so sliding works properly
+        }
+        
+        // Use setTimeout to avoid instant appearance of new page
+        // Kind of jimmy-rigged, but I don't know how to solve the root problem :(
+        setTimeout(() => {
+            if (slideLeft) {
+                $(targetPageId).removeClass("hidden");
+                $(targetPageId).css("left", (($(window).width() - dx) + "px"));
+                $(prevPageId).css("left", (-1 * dx + "px"));
+            } else {
+                $(targetPageId).removeClass("hidden");
+                $(targetPageId).css("left", ((-1 * dx) + "px"));
+                $(prevPageId).css("left", (dx + "px"));
+                $(prevPageId).addClass("page_right");
+            }
+        }, 1);
+    }
     
     /**
      * Return the div_page element id to compare or set the pages Map key.
-     * 
+     *
      * @return {String} the id value of the div_page, if present. Otherwise,
      * "" will return
-     * 
+     *
      * @example
      * verifyPageKey("<h1>Title</h1>  <div id=timer class =div_page><p>Content here</p></div>")
      *     Returns -->  "timer"
-     * 
+     *
      * if("somePageKey" != verifyPageKey("<html content goes here>")) { console.log("ERROR"); }
-     * 
+     *
      * @param {String} pageHtml the html content for the page
      */
-    this.verifyPageKey = function(pageHtml) {
+    verifyPageKey(pageHtml) {
         // Find the id of the div to compare to page Map key
         let divIndex = pageHtml.indexOf("<div");
         let idIndex = pageHtml.indexOf("id=", divIndex);
         let divId = "";
-        
         // Invalidate the index if it is outside of the <div ... > tag
         if(idIndex > pageHtml.indexOf(">", divIndex)) {
             idIndex = -1;
         }
-        
-        if ((divIndex != -1) && (idIndex != -1)) {
-            endIdIndex = pageHtml.indexOf("\"", idIndex + 4); // +3 to remove quotes from id="
+        if((divIndex != -1) && (idIndex != -1)) {
+            let endIdIndex = pageHtml.indexOf("\"", idIndex + 4); // +3 to remove quotes from id="
             // +3 to remove "id="
             divId = pageHtml.substring(idIndex + 3, endIdIndex);
             divId = divId.replace(/\"/g, "").replace("#", ""); // Remove all quotes and hashtags
@@ -233,6 +269,7 @@ function PageTransition(firstPageHtml = "", firstPageKey = "") {
             console.log("[PageTransition][verifyPageKey()]: Div or id index was invalid");
             return "";
         }
+        return "";
     }
     
 }
