f63cafe59715dce9da07776a6dcb7474d1986628
Matt <>
2020-06-18 15:29:37 -0400

    added event results & sorting

--- www/js/stats.js
+++ www/js/stats.js
@@ -14,7 +14,7 @@ class Stats extends Page {
             <div id="landingPage">
                 <br><br>
                 <h1>Stats</h1><br><br>
-                <div class="button_box"></div>
+                <div class="button_box"></div><br><br>
             </div>
         `);
 
@@ -26,21 +26,19 @@ class Stats extends Page {
             </div><br><br>
 
             <div class="table_container sportwatch_selector">
-                <div id="sort_alphabet">Az</div>
+                <div id="sort_alphabet">A-z</div>
                 <div id="sort_times">0-9</div>
                 <div id="sort_gender">M/F</div>
             </div><br><br>
 
-            <div class="table_container column_names">
-                <div>Name</div>
-                <div>Best</div>
-                <div>Average</div>
-                <div>Worst</div>
-            </div>
-
-            <div class="button_box">
-                
-            </div>
+            <table id="event_results">
+                <tr class="column_names">
+                    <th>Name</th>
+                    <th>Best</th>
+                    <th>Average</th>
+                    <th>Worst</th>
+                </tr>
+            </table>
         </div>
         `);
 
@@ -91,50 +89,157 @@ class Stats extends Page {
             ButtonGenerator.generateButtonsFromDatabase("#statsPage #landingPage .button_box", events, (event) => {
                 this.startEventPage(event);
             }, ["gender", "unit", "is_relay", "timestamp"]);
+
+            let addButton = ButtonGenerator.generateButton({ text: "Add Event", class: "add_event_button" }, () => {
+                this.startAddEventPage();
+            });
+
+            $("#statsPage #landingPage").append(addButton);
         });
     }
 
-    startAddEventPage(event) {
+    startAddEventPage() {
         // TODO: implement
-
-        $("#statsPage #eventPage #event_name").html(event.event_name);
-
 
     }
 
     startEventPage(event) {
+
         this.pageTransition.slideLeft("eventPage");
+
+        this.clearResultsTable();
+
         $("#statsPage #eventPage #event_name").html(event.event_name);
 
         $("#statsPage #eventPage #back_button_event").bind("touchend", (e) => {
             this.pageTransition.slideRight("landingPage");
         });
 
+        $("#statsPage #eventPage #sort_alphabet").unbind("touchend");
+        $("#statsPage #eventPage #sort_times").unbind("touchend");
+        $("#statsPage #eventPage #sort_gender").unbind("touchend");
+
+        // Sort alphabetically
         $("#statsPage #eventPage #sort_alphabet").bind("touchend", (e) => {
             $("#statsPage #eventPage #sort_alphabet").addClass("selected");
             $("#statsPage #eventPage #sort_times").removeClass("selected");
             $("#statsPage #eventPage #sort_gender").removeClass("selected");
+
+            this.generateAthleteTimes(event, "A-z");
         });
 
+        // sort based on fasted time
         $("#statsPage #eventPage #sort_times").bind("touchend", (e) => {
             $("#statsPage #eventPage #sort_times").addClass("selected");
             $("#statsPage #eventPage #sort_alphabet").removeClass("selected");
             $("#statsPage #eventPage #sort_gender").removeClass("selected");
 
+            this.generateAthleteTimes(event, "0-9");
         });
 
+        // sort based on gender
         $("#statsPage #eventPage #sort_gender").bind("touchend", (e) => {
             $("#statsPage #eventPage #sort_gender").addClass("selected");
             $("#statsPage #eventPage #sort_alphabet").removeClass("selected");
             $("#statsPage #eventPage #sort_times").removeClass("selected");
 
+            this.generateAthleteTimes(event, "M/F");
         });
 
-        this.dbConnection.selectValues("SELECT * from event_result WHERE id_event = ?", [event.rowid]).then((results) => {
-            console.log(JSON.stringify(results));
-        }).catch(() => {
+        this.generateAthleteTimes(event);
+    }
+
+    generateAthleteTimes(event, order) {
+
+        this.clearResultsTable();
+
+        let query = `
+            SELECT * FROM event_result
+            INNER JOIN athlete ON event_result.id_athlete = athlete.rowid
+            WHERE event_result.id_event = ?
+        `;
+
+        this.dbConnection.selectValues(query, [event.rowid]).then((results) => {
+
+            let athletes = this.constructAthleteTimeArray(results, order);
+
+            for (let i = 0; i < athletes.length; i++) {
+
+                if (athletes[i] === null || athletes[i] === undefined) {
+                    continue;
+                }
+
+                console.log(JSON.stringify(athletes[i]));
+
+                let name = athletes[i].fname + "\t" + athletes[i].lname;
+                let min = Math.min(...athletes[i].values);
+                let max = Math.max(...athletes[i].values);
+                let average = (athletes[i].values.reduce((a, b) => a + b, 0) / athletes[i].values.length).toFixed(2);
+
+                let info_box;
+
+                if (athletes[i].gender == 'm') {
+                    info_box = $("<tr>", { class: "male_color" });
+                } else if (athletes[i].gender == 'f') {
+                    info_box = $("<tr>", { class: "female_color" });
+                } else {
+                    info_box = $("<tr>");
+                }
+
+                info_box.append($("<td>", { text: name }));
+                info_box.append($("<td>", { text: max }));
+                info_box.append($("<td>", { text: average }));
+                info_box.append($("<td>", { text: min }));
+
+                $("#statsPage #eventPage #event_results").append(info_box);
+            }
 
         });
+    }
+
+    constructAthleteTimeArray(rows, order) {
+
+        let array = [];
+
+        let athlete_ids = [];
+
+        for (let i = 0; i < rows.length; i++) {
+            if (athlete_ids.includes(rows.item(i).id_athlete)) {
+                array[rows.item(i).id_athlete].values.push(rows.item(i).value);
+
+            } else if (!athlete_ids.includes(rows.item(i).id_athlete)) {
+                athlete_ids.push(rows.item(i).id_athlete);
+                array[rows.item(i).id_athlete] = rows.item(i);
+                array[rows.item(i).id_athlete].values = [rows.item(i).value];
+            }
+        }
+
+        if (order == "A-z") {
+            array.sort((a, b) => (a.lname > b.lname) ? 1 : ((b.lname > a.lname) ? -1 : 0));
+        } else if (order == "0-9") {
+            array.sort((a, b) => (Math.max(...a.values) > Math.max(...b.values)) ? 1 : ((Math.max(b.values) > Math.max(a.values)) ? -1 : 0));
+        } else if (order == "M/F") {
+            array.sort((a, b) => (a.gender > b.gender) ? 1 : ((b.gender > a.gender) ? -1 : 0));
+        }
+
+        console.log(JSON.stringify(array));
+        console.log(array.length);
+
+        return array;
+    }
+
+    /**
+     * will remove all of the generated entries in the results
+     */
+    clearResultsTable() {
+        $("#statsPage #eventPage #event_results").html(`
+            <tr class="column_names">
+                <th>Name</th>
+                <th>Best</th>
+                <th>Average</th>
+                <th>Worst</th>
+            </tr>
+        `);
     }
 
     addEvent(event) {
