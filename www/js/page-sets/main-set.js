ada78d9abf43ac1831a1d76dce32fdcd2c58fe34
Matt <19471665+LawnmowerDave@users.noreply.github.com>
2020-07-20 17:13:07 -0400

    undo content policy change

--- www/js/page-sets/main-set.js
+++ www/js/page-sets/main-set.js
@@ -22,7 +22,7 @@ class MainSet extends PageSet {
     
     constructPages() {
         // TODO: sorry seth, I'm testing the team page, just change TeamLanding back
-        this.pageArray = [Stopwatch, Stats, Team, Account].map((page, i) => new page(i, this));
+        this.pageArray = [Stopwatch, Stats, TeamLanding, Account].map((page, i) => new page(i, this));
         this.pageArray.forEach((pageObj, pageIndex) => {
             let shouldShow = false; // Should be page be visible at start? (only for first page)
             if (pageIndex == 0) {
@@ -35,7 +35,7 @@ class MainSet extends PageSet {
         });
         
         // Include this here so it's easier to enable / disable later
-        this.navbar.initNavbar(this.switchPage.bind(this), ["Stopwatch", "Stats", "Team", "Account"]);
+        this.navbar.initNavbar(this.switchPage.bind(this), ["Stopwatch", "Stats", "TeamLanding", "Account"]);
         this.disable(); // Disable until enabled
     }
     
